883ca0297a3e6b471c63dc18436b5e495dfc95ab
Brent McKinney <brentemckinney@gmail.com>
2013-04-22 15:48:51 -0500

    commit after crazy computer issues

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,9 +11,15 @@ describe "Static pages" do
 				:text => 'Sample App')
 		end
 
-		it "should have the right title" do
+		it "should have the base title" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title',
 				:text => "#{base_title} | Home")
 		end
 	end
